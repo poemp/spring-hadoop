b615c96537b6f285dcecffb91d9a879a7e854f5f
luyanwen <xue_2013@sina.com>
2019-04-01 18:04:17 +0800

    inti spring boot

---
+++ src/main/java/org/poem/SpringHadoopApplication.java
@@ -0,0 +1,16 @@
+package org.poem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class SpringHadoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHadoopApplication.class, args);
+    }
+
+}
