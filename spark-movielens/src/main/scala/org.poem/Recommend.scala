e282b9668726247ff4afb15553eea3039f127e4d
luyanwen <xue_2013@sina.com>
2019-05-22 23:59:08 +0800

    修改测试

--- spark-movielens/src/main/scala/org.poem/Recommend.scala
+++ spark-movielens/src/main/scala/org.poem/Recommend.scala
@@ -2,7 +2,9 @@ package org.poem
 
 import java.util.logging.{Level, Logger}
 
-import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.recommendation.{MatrixFactorizationModel, Rating}
+import org.apache.spark.rdd.RDD
 
 class Recommend {
 
@@ -28,5 +30,35 @@ class Recommend {
         //RecommonUsers(modle, movieTitle, inputMovieId.toInt)
       }
     }
+  }
+
+  /**
+    * 准备数据
+    * @return
+    */
+  def PrepareData(): (RDD[Rating], Map[Int, String]) ={
+    // ---------------- 1. 创建用户评分数据 --------------
+    val sc = new SparkContext(new SparkConf().setAppName("wordCount").setMaster("local[4]"))
+    println("开始读取文件")
+    //UserID::Gender::Age::Occupation::Zip-code
+    val rowUserData = sc.textFile("/root/workspace/recommend/data/ml-1m/users.dat")
+    //UserID::MovieID::Rating::Timestamp
+    val ratingsData = sc.textFile("/root/workspace/recommend/data/ml-1m/ratings.dat")
+
+    //训练出来了
+    val rawRatings = ratingsData.map(_.split("::").take(3)).map { case Array(user, movice, rating) => Rating(user.toInt, movice.toInt, rating.toDouble) }
+    println("共计:" + rawRatings.count().toString + " 条 ratings")
+    // ---------------- 2. 创建电影id 和名称的映射 --------------
+    println("开始读取电影数据中 .... ")
+    //MovieID::Title::Genres
+    val moviesData = sc.textFile("/root/workspace/recommend/data/ml-1m/movies.dat")
+    val moviesRDD = moviesData.map(_.split("::").take(2))
+      .map(array => (array(0).toInt, array(1))).collect().toMap
+    // ---------------- 3. 显示记录数量 --------------
+    val numRatings = rawRatings.count()
+    val numUsers = rawRatings.map(_.user).distinct().count()
+    val numMovies = rawRatings.map(_.product).distinct().count()
+    println(" 共计："+numRatings.toString + " user: " + numUsers.toString + "movies:" + numMovies )
+    (rawRatings, moviesRDD)
   }
 }
